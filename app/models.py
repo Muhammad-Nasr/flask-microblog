5d88f592e339cdaac18688ca939848def94fdc01
Muhammad Nasr <88571996+Muhammad-Nasr@users.noreply.github.com>
2022-03-21 15:25:53 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -58,7 +58,7 @@ followers = db.Table('followers',
 class User(PaginatedAPIMixin, UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120))
+    email = db.Column(db.String(120), default='example@gmail.com')
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
